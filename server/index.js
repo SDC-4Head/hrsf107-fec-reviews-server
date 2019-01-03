8eecafa20f340f2bd7b20248c4bb517e6cdaaf45
James Tsukamoto <jaytsuk@gmail.com>
2019-01-02 16:56:25 -0800

    Complete bare minimum requirements of proxy server exercise

--- server/index.js
+++ server/index.js
@@ -6,8 +6,20 @@ const port = process.env.PORT || 3000;
 
 
 app.use(morgan('dev'));
+
+// Reviews - James
 app.use('/api/reviews/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
 app.use('/api/ratings/rooms/:roomid', proxy({target: 'http://localhost:3124'}));
+
+// Image Gallery - Josh
+app.use('/rooms/:id/photos', proxy({target: 'http://localhost:1337'}));
+
+// Booking - Kevin
+app.use('/api/rooms/:id', proxy({target: 'http://localhost:8080'}));
+
+// Recommendations - Eric
+app.use('/house', proxy({target: 'http://localhost:3123'}));
+
 app.use('/rooms/:roomid', express.static("./public"));
 
 app.listen(port, () => {
