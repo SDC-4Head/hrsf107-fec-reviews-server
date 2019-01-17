9685b6199170f5ba69fafc0effb83fb006c30908
Francis Distor <francis.distor@gmail.com>
2019-01-17 11:12:26 -0800

    Stress test for get request to proxy,.

--- k6/get_load.js
+++ k6/get_load.js
@@ -2,8 +2,8 @@ import http from "k6/http";
 import { sleep, check } from "k6";
 
 export let options = {
-  vus: 10,
-  duration: '10s',
+  vus: 1000,
+  duration: '300s',
 }
 
 export default function() {
